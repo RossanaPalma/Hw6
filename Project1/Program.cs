e2a04e289cd1b32dbf067a8f6a74d6e176c9d225
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2021-10-04 20:57:39 -0700

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,50 @@
+﻿/// Chapter No. 13 - Homework6_Part_1
+/// File Name: homework6.java
+/// @author: Rossana Palma
+/// Date:  October 3, 2021
+///
+/// Problem Statement: Write a program that creates an array with 10 items in it and creates a copy of it 
+/// of the same length and then place the values in the copy.
+/// 
+/// Overall Plan:
+/// 1) Create an array with 10 items
+/// 2) Create a second array with same length and empty
+/// 3) Use for loop read values from the original array and place them in the second array
+/// 4) Display the contents of both arrays
+
+
+
+
+using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SIZE = 10;
+
+            //Create initial array
+            int[] initialArray = new int[SIZE] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
+            //Second array: same length as the first and empty
+            int[] copyArray = new int[initialArray.Length];
+
+            //Use a loop to read values from the original array and place them in the new array.
+            for(int i = 0; i <initialArray.Length; i++)
+            {
+                copyArray[i] = initialArray[i];
+
+                //Print out the contents of both arrays
+                
+                Console.Write("\tCopy array: ");
+                Console.Write(copyArray[i] + " ");
+                Console.Write("\tInitial array: ");
+                Console.WriteLine(initialArray[i] + " ");
+            }
+
+                      
+        }
+    }
+}
---
+++ Project2/Program.cs
@@ -0,0 +1,63 @@
+﻿/// Chapter No. 13 - Homework6_Part_2
+/// File Name: homework6.java
+/// @author: Rossana Palma
+/// Date:  October 3, 2021
+///
+/// Problem Statement: Write a program to reverse an array. Use three methods: one to create the array, one to reverse the array, 
+/// and one to print the array at the end. 
+/// 
+/// Overall Plan:
+/// 1) Create GenerateNumbers method to create an array of 10 numbers
+/// 2) Create Reverse method to reverse the array
+/// 3) Create PrintNumbers method to print out the items in it.
+/// 4) Invoke the methods in Main method
+/// 5) Display results on the screen
+
+
+
+using System;
+
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = GenerateNumbers();
+            Reverse(numbers);
+            PrintNumbers(numbers);
+
+            Console.ReadLine();
+        }
+        static int[] GenerateNumbers()
+        {   
+            //Create an array using loop
+            int[] array = new int[10];
+            for(int i = 0; i < 10; i++ )
+            {
+                array[i] = i + 1;
+            }
+            return array; // return an array of 10 numbers
+        }
+        static void Reverse(int[] array)
+        {
+            //Use a for loop, starting at 0 and going up the length of the array / 2
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                //place the value of one variable in a temporary location to make the swap
+                int temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                temp = array[array.Length - i - 1];
+            }
+        }
+
+        static void PrintNumbers(int[] array)
+           {
+             foreach(int i in array)
+                {
+                    Console.WriteLine(i + " " );
+                }
+           }
+    }
+}
